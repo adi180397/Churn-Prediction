f62c33bb455127ace559a8d778b387487de5c4c8
Aditya Kumar <adi.763192@gmail.com>
2026-01-04 23:54:19 +0530

    utils and requirement setup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from src.mlProject.Logging import get_logger
+
 logger=get_logger(__name__)
 logger.info("first logging msg for the testing pupose")
---
+++ test_utils.py
@@ -0,0 +1,3 @@
+from src.mlProject.utils.common import read_yaml
+from pathlib import Path
+print(read_yaml(Path("config/config.yaml")))
