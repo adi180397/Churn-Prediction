326c44f0ff46efa14d0221d8e3a5f66fc04c1601
Aditya Kumar <adi.763192@gmail.com>
2026-01-08 11:04:21 +0530

    data ingestion and eda on notebook done

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from src.mlProject.Logging import get_logger
 
 logger=get_logger(__name__)
-logger.info("first logging msg for the testing pupose")
+from src.mlProject.pipeline.data_ingestion_stage import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME='Data_ingestion_training_pipeline'
+
+if __name__=='__main__':
+    
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
